c405b36c0a6efb4880b350675d86b536e3bf8239
Marc Dwyer <envdia@gmail.com>
2018-12-01 20:10:37 -0600

    higher interval

--- init/getstreams.js
+++ init/getstreams.js
@@ -19,7 +19,6 @@ const fs = require('fs');
       {name: 'Andy', channelId: 'UCovb8rgpCANx6nwDwnW0Uqg'},
       {name: 'Code', channelId: 'UCvjgXvBlbQiydffZU7m1_aw'},
       {name: 'Joe', channelId: 'UCzQUP1qoWDoEbmsQxvdjxgQ'},
-      {name: 'Me', channelId: 'UCBawnZIFCiN_WNvsseGyjYA'},
       {name: 'Nasa', channelId: 'UCLA_DiR1FfKNvjuUpBHmylQ'},
       {name: 'CBS', channelId: 'UC8p1vwvWtl6T73JiExfWs1g'}
     ];
